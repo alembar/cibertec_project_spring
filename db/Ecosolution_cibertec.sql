ffe050375f6c928f53ee2387185b712f711ff3a8
alem guillermo bar villalobos <abar@hacklibre.com>
2023-08-12 00:38:34 -0500

    db

---
+++ db/Ecosolution_cibertec.sql
@@ -0,0 +1,140 @@
+-- MySQL dump 10.13  Distrib 8.0.32, for Linux (x86_64)
+--
+-- Host: 127.0.0.1    Database: Ecosolution_cibertec
+-- ------------------------------------------------------
+-- Server version	8.0.33-0ubuntu0.22.04.4
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `cliente`
+--
+
+DROP TABLE IF EXISTS `cliente`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `cliente` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `razon_social` varchar(70) DEFAULT NULL,
+  `ruc` varchar(12) DEFAULT NULL,
+  `correo_electronico` varchar(200) DEFAULT NULL COMMENT '	',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `cliente`
+--
+
+LOCK TABLES `cliente` WRITE;
+/*!40000 ALTER TABLE `cliente` DISABLE KEYS */;
+INSERT INTO `cliente` VALUES (1,'Hacklibre EIRL','20604175446',NULL);
+/*!40000 ALTER TABLE `cliente` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `cotizacion`
+--
+
+DROP TABLE IF EXISTS `cotizacion`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `cotizacion` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `nombre_cotizacion` varchar(200) NOT NULL,
+  `nro_cotizacion` int NOT NULL,
+  `tiempo_entrega` varchar(200) NOT NULL,
+  `asunto_cotizacion` varchar(200) NOT NULL,
+  `estado` int NOT NULL DEFAULT '0',
+  `id_cliente` int NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `fk_cotizacion_1_idx` (`id_cliente`),
+  CONSTRAINT `fk_cotizacion_1` FOREIGN KEY (`id_cliente`) REFERENCES `cliente` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `cotizacion`
+--
+
+LOCK TABLES `cotizacion` WRITE;
+/*!40000 ALTER TABLE `cotizacion` DISABLE KEYS */;
+INSERT INTO `cotizacion` VALUES (1,'cotizacion 1',1,'4 dias calendario','asunto de la cotizacion',0,1),(2,'cotizacion 1',2,'4 dias calendario','asunto de la cotizacion',0,1);
+/*!40000 ALTER TABLE `cotizacion` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `cuerpo_cotizacion`
+--
+
+DROP TABLE IF EXISTS `cuerpo_cotizacion`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `cuerpo_cotizacion` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `cantidad` int DEFAULT NULL,
+  `id_cotizacion` int DEFAULT NULL,
+  `id_item` int DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `fk_cuerpo_cotizacion_1_idx` (`id_cotizacion`),
+  KEY `fk_cuerpo_cotizacion_2_idx` (`id_item`),
+  CONSTRAINT `fk_cuerpo_cotizacion_1` FOREIGN KEY (`id_cotizacion`) REFERENCES `cotizacion` (`id`),
+  CONSTRAINT `fk_cuerpo_cotizacion_2` FOREIGN KEY (`id_item`) REFERENCES `item` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `cuerpo_cotizacion`
+--
+
+LOCK TABLES `cuerpo_cotizacion` WRITE;
+/*!40000 ALTER TABLE `cuerpo_cotizacion` DISABLE KEYS */;
+INSERT INTO `cuerpo_cotizacion` VALUES (1,10,1,1),(2,2,1,2);
+/*!40000 ALTER TABLE `cuerpo_cotizacion` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `item`
+--
+
+DROP TABLE IF EXISTS `item`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `item` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `nombre_item` varchar(100) DEFAULT NULL,
+  `description` varchar(1000) DEFAULT NULL,
+  `precio_unitario` double DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `item`
+--
+
+LOCK TABLES `item` WRITE;
+/*!40000 ALTER TABLE `item` DISABLE KEYS */;
+INSERT INTO `item` VALUES (1,'Servicio 001','Servicio para poder realizar la matriz conesa con cada etapa y fase del proceso de construcción',440.2),(2,'Servicio 002','Servicio de impacto ambiental',950.1);
+/*!40000 ALTER TABLE `item` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2023-08-12  0:36:09
